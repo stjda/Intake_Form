9f32f152001782d3ed15551e3def98d07b7b6435
guymorganb <guymorganb@gmail.com>
2024-09-04 16:32:35 -0500

    altered the /dist

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,21 +8,21 @@ export default defineConfig({
       "@mui/material/Box",
     ],
  },
-  logLevel: 'info',
-  server: {
-    proxy: {
-      '/api': {
-           target: 'http://34.135.9.49:3000',
-           changeOrigin: true,
-           secure: false,
-       }
-    },
-  },
+  // logLevel: 'info',
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //          target: 'http://34.135.9.49:3000',
+  //          changeOrigin: true,
+  //          secure: false,
+  //      }
+  //   },
+  // },
   plugins: [
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'robots.txt', '*.jpg', '*.png'],
+      includeAssets: ['favicon.svg', 'robots.txt', '*.jpg', '*.png'], // Include all assets to be cached
       manifest: {
         name: 'STJDA Intake',
         short_name: 'STJDA',
@@ -30,33 +30,36 @@ export default defineConfig({
         theme_color: '#ffffff',
         background_color: '#ffffff',
         display: 'standalone',
-        start_url: '/STJDA_Intake/',
-        id: '/STJDA_Intake/',
-        scope: '/STJDA_Intake/',
+        //start_url: 'https://guyycodes.github.io/STJDA_Intake/', // start URL for GitHub Pages
+        id: '/STJDA_Intake/', //  a consistent ID for the app
+        scope: '/STJDA_Intake/', // the scope to restrict what URLs are considered part of the app
         icons: [
+          // Include square icons as required by most devices
           {
-            src: '/STJDA_Intake/192x192Icon.png',
+            src: '192x192Icon.png', // path to the icon
             sizes: '192x192',
             type: 'image/png',
-            purpose: 'any',
+            purpose: 'any', // Can be used as app icon and shortcut icon
           },
+          // More icon sizes can be included here, for example, 512x512
           {
-            src: '/STJDA_Intake/512x512Icon.png',
+            src: '512x512Icon.png', // Provide the correct path to the icon
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any',
           },
         ],
         screenshots: [
+          // Include screenshots to provide a preview during PWA installation
           {
-            src: '/STJDA_Intake/Screenshot1280x720.png',
+            src: 'Screenshot1280x720.png', // Path to the desktop screenshot
             sizes: '1280x720',
             type: 'image/png',
             label: 'Desktop Screenshot',
             form_factor: 'wide',
           },
           {
-            src: '/STJDA_Intake/Screenshot640x1136.png',
+            src: 'Screenshot640x1136.png', // Path to the mobile screenshot these sizes must be exact
             sizes: '640x1136',
             type: 'image/png',
             label: 'Mobile Screenshot',
@@ -64,13 +67,11 @@ export default defineConfig({
           },
         ],
       },
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,jpg}'],
-      },
     }),
   ],
   build: {
-    outDir: 'dist',
-    base: '/STJDA_Intake/',
+    outDir: 'dist', // Default output directory
+    base: '/STJDA_Intake/', // Base public path set to match the GitHub repository name
+    sourcemap: true, 
   },
 });
