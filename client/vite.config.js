5a120644f54383d8a2707417ac733c9ff7a516fe
guymorganb <guymorganb@gmail.com>
2024-09-04 15:42:26 -0500

    altered the /dist

--- client/vite.config.js
+++ client/vite.config.js
@@ -30,20 +30,20 @@ export default defineConfig({
         theme_color: '#ffffff',
         background_color: '#ffffff',
         display: 'standalone',
-        start_url: '/STJDA_Intake/', // start URL for GitHub Pages
+        start_url: 'https://guyycodes.github.io/STJDA_Intake/', // start URL for GitHub Pages
         id: '/STJDA_Intake/', //  a consistent ID for the app
         scope: '/STJDA_Intake/', // the scope to restrict what URLs are considered part of the app
         icons: [
           // Include square icons as required by most devices
           {
-            src: '/192x192Icon.png', // path to the icon
+            src: '192x192Icon.png', // path to the icon
             sizes: '192x192',
             type: 'image/png',
             purpose: 'any', // Can be used as app icon and shortcut icon
           },
           // More icon sizes can be included here, for example, 512x512
           {
-            src: '/512x512Icon.png', // Provide the correct path to the icon
+            src: '512x512Icon.png', // Provide the correct path to the icon
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any',
@@ -52,14 +52,14 @@ export default defineConfig({
         screenshots: [
           // Include screenshots to provide a preview during PWA installation
           {
-            src: '/Screenshot1280x720.png', // Path to the desktop screenshot
+            src: 'Screenshot1280x720.png', // Path to the desktop screenshot
             sizes: '1280x720',
             type: 'image/png',
             label: 'Desktop Screenshot',
             form_factor: 'wide',
           },
           {
-            src: '/Screenshot640x1136.png', // Path to the mobile screenshot these sizes must be exact
+            src: 'Screenshot640x1136.png', // Path to the mobile screenshot these sizes must be exact
             sizes: '640x1136',
             type: 'image/png',
             label: 'Mobile Screenshot',
